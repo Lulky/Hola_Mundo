3da5c5eb9ab08d4edae05fb0044065b1e8d4fece
Lulky <lukyluxiano.12@gmail.com>
2022-02-28 20:44:50 -0500

    Hola_Mundo

---
+++ hola_mundo.py
@@ -0,0 +1,14 @@
+mundo = "Hola, mundo"
+print(mundo)
+nombre = "Luciano"
+print("!Hola", nombre, "!")
+print("!Hola" + nombre + "!")
+num = 8
+print("Hola", num)
+# print("Hola"+ num)
+print("Hola"+ str(num))
+comida_uno = "chaufa"
+comida_dos = "Ají de Gallina"
+print("Amo comer {} y {}".format(comida_uno, comida_dos))
+print(f"Amo comer {comida_uno} y {comida_dos}")
+
